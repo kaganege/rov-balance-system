02dfff9b3c1e5528b2a07a75f5831e36aa814d25
kaganege <kaganegeozkan@gmail.com>
2023-11-20 21:51:04 +0300

    some bug fixes

--- build.ps1
+++ build.ps1
@@ -1,18 +1,22 @@
-$platformio = "$HOME\.platformio\penv\Scripts\platformio.exe"
+if ($null -eq (Get-Command platformio -ErrorAction SilentlyContinue)) {
+  $platformio = "$HOME\.platformio\penv\Scripts\platformio.exe"
 
-if (Test-Path -Path $platformio -PathType Leaf) {
-  & $platformio run
-
-  if (Test-Path .\build -PathType Container) {
-    Remove-Item .\build\*
+  if (Test-Path -Path $platformio -PathType Leaf) {
+    Set-Alias platformio $platformio
   }
   else {
-    New-Item build -ItemType Directory
+    Write-Host "Platformio can not found!"
+    Exit 1
   }
-  
-  Get-Item .\.pio\build\rpipicow\firmware.* | Move-Item -Destination .\build\
+}
+
+platformio run
+
+if (Test-Path .\build -PathType Container) {
+  Remove-Item .\build\*
 }
 else {
-  Write-Host "Platformio can not found!"
-  Read-Host -Prompt "Press Enter to continue..."
+  New-Item build -ItemType Directory
 }
+
+Get-Item .\.pio\build\rpipicow\firmware.* | Move-Item -Destination .\build\
