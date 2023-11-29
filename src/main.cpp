34c0e575ffdfdd8c2accdd3465bc743fb729f0b6
kaganege <kaganegeozkan@gmail.com>
2023-11-30 01:16:32 +0300

    improved pot stability

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,79 @@
 #include <Arduino.h>
 #include <ESC.h>
+#include <Gyro.h>
 
 #define FIT(value, _min, _max) min(max(value, _min), _max)
+#define OR(condition, default) condition ? condition : default
+
+#define POT_PIN 26
+#define POT_MIN 50
+#define POT_MAX 4090
 
 #define ESC_PIN 2
-#define POT_PIN 26
 
 // min pulse width 1160
 
 ESC esc;
-int power = 0;
-int pot_min, pot_max;
-bool wait_for_zero = true;
+Gyro gyro;
 
-// template <typename T>
-// T fit(T value, T _min, T _max)
-// {
-//   return min(max(value, _min), _max);
-// }
+int power = 0;
+// int pot_min, pot_max;
+bool wait_for_zero = true;
 
 void setup()
 {
   analogReadResolution(12); // 12 bit (0 - 4096)
-
-  // We are driving high GPIO 23 because forcing the power supply into PWM mode reduces ADC noise.
-  pinMode(23, OUTPUT);
-  digitalWrite(23, HIGH);
-
   pinMode(POT_PIN, INPUT);
-  // esc.attach(ESC_PIN);
 
   Serial.begin();
+
+  esc.attach(ESC_PIN);
+  gyro.init();
+
+  gyro.waitUntilConnect();
 }
 
 void loop()
 {
+  if (BOOTSEL)
+  {
+    rp2040.rebootToBootloader();
+  }
+
   int pot_value = analogRead(POT_PIN);
 
-  if (!pot_min && BOOTSEL)
-  {
-    pot_min = pot_value + 10;
+  // if (!pot_min && BOOTSEL)
+  // {
+  //   pot_min = pot_value + 10;
 
-    while (BOOTSEL)
-      ;
+  //   while (BOOTSEL)
+  //     ;
+  // }
+  // else if (!pot_max && BOOTSEL)
+  // {
+  //   pot_max = pot_value - 10;
+
+  //   while (BOOTSEL)
+  //     ;
+  // }
+  pot_value = FIT(pot_value, POT_MIN, POT_MAX);
+
+  if (wait_for_zero)
+  {
+    if (pot_value == POT_MIN)
+      wait_for_zero = false;
   }
-  else if (!pot_max && BOOTSEL)
+  else
   {
-    pot_max = pot_value - 10;
+    power = map(pot_value, POT_MIN, POT_MAX, 0, 1000);
 
-    while (BOOTSEL)
-      ;
-  }
-  else if (pot_min && pot_max)
-  {
-    pot_value = map(FIT(pot_value, pot_min, pot_max), pot_min, pot_max, 0, 1000);
-
-    if (wait_for_zero)
+    if (esc.getPower() != power)
     {
-      if (pot_value == 0)
-        wait_for_zero = false;
-    }
-    else
-    {
-      power = pot_value;
-
-      if (esc.getPower() != power)
-      {
-        esc.setPower([](int currentPower)
-                     { return currentPower < power ? currentPower + 1 : currentPower - 1; });
-        delay(1);
-      }
+      esc.setPower([](int currentPower)
+                   { return currentPower < power ? currentPower + 1 : currentPower - 1; });
     }
   }
-  Serial.printf("Potentiometer value: %d\n", pot_value);
+  Serial.printf("Potentiometer value: %d, Motor power: %d\n", pot_value, power);
 
-  delayMicroseconds(500);
+  delay(1);
 }
